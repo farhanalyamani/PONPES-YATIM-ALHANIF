f0e2bb924a08a1b4cfa964af4676db3f05611047
farhanalyamani <farhanef180503@gmail.com>
2026-01-13 16:36:29 +0700

    update fitur login

--- js/login.js
+++ js/login.js
@@ -14,9 +14,19 @@ document.addEventListener("DOMContentLoaded", function () {
   });
 
   form.addEventListener("submit", function (e) {
-    e.preventDefault(); // ⛔ stop reload
+  e.preventDefault();
+
+  document.getElementById("btnText").classList.add("d-none");
+  document.getElementById("btnLoading").classList.remove("d-none");
+
+  setTimeout(() => {
     login();
-  });
+
+    document.getElementById("btnText").classList.remove("d-none");
+    document.getElementById("btnLoading").classList.add("d-none");
+  }, 600);
+});
+
 
 });
 
@@ -28,12 +38,40 @@ function login(){
     sessionStorage.setItem("userLogin", "true");
     window.location.href = "pendaftaran.html";
   }
-  else if(role === "admin"){
-    if(pass === "admin2026"){
+    else if (role === "admin") {
+    if (pass === "admin2026") {
       sessionStorage.setItem("adminLogin", "true");
       window.location.href = "admin-psya-8f92.html";
     } else {
-      alert("❌ Password admin salah!");
+      const errorMsg = document.getElementById("errorMsg");
+      const card = document.querySelector(".login-card");
+
+      errorMsg.classList.remove("d-none");
+
+      card.classList.add("shake");
+      setTimeout(() => card.classList.remove("shake"), 300);
     }
   }
 }
+
+function showError(msg){
+  const box = document.getElementById("errorBox");
+  box.textContent = msg;
+  box.classList.remove("d-none");
+
+  setTimeout(() => {
+    box.classList.add("d-none");
+  }, 3000);
+}
+const togglePassword = document.getElementById("togglePassword");
+const passwordInput = document.getElementById("password");
+
+togglePassword.addEventListener("click", () => {
+  const type = passwordInput.type === "password" ? "text" : "password";
+  passwordInput.type = type;
+
+  togglePassword.innerHTML =
+    type === "password"
+      ? '<i class="fas fa-eye"></i>'
+      : '<i class="fas fa-eye-slash"></i>';
+});
